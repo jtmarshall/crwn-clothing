e74c705d8bc14b53207ddf800474f48bbf77f1c6
Jordan <jordantmarshall3@gmail.com>
2019-09-18 19:08:40 -0500

    add form-input components with styles

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-const HatsPage = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>HATS PAGE </h1>
-    </div>
-  );
-};
+// const HatsPage = props => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>HATS PAGE </h1>
+//     </div>
+//   );
+// };
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
